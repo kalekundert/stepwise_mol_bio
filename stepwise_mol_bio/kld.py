24095940829663ebd133c77b7dda262cac72a550
Kale Kundert <kale@thekunderts.net>
2020-03-23 17:59:27 -0400

    Add an inverse PCR protocol.

--- stepwise_mol_bio/kld.py
+++ stepwise_mol_bio/kld.py
@@ -15,31 +15,52 @@ Arguments:
 
 import docopt
 import stepwise
+import autoprop
 from inform import plural
 
-args = docopt.docopt(__doc__)
-protocol = dirty_water.Protocol()
+@autoprop
+class Kld:
 
-kld = stepwise.Reaction('''\
-Reagent               Stock        Volume  Master Mix
-================  =========   ===========  ==========
-water                         to 10.00 μL         yes
-T4 ligase buffer        10x       1.00 μL         yes
-T4 PNK              10 U/μL       0.25 μL         yes
-T4 DNA ligase      400 U/μL       0.25 μL         yes
-DpnI                20 U/μL       0.25 μL         yes
-PCR product        50 ng/μL       1.50 μL
-''')
+    def __init__(self):
+        self.num_reactions = 1
 
-kld.num_reactions = eval(args['<num_reactions>'])
-kld.extra_fraction = 15
+    @classmethod
+    def from_docopt(cls, *docopt_args, **docopt_kwargs):
+        args = docopt.docopt(*docopt_args, **docopt_kwargs)
 
-protocol += """\
-Run {plural(kld.num_reactions):# ligation reaction/s}:
+        kld = cls()
+        kld.num_reactions = eval(args['<num_reactions>'])
 
-{kld}
+        return kld
+
+    def get_reaction(self):
+        kld = stepwise.MasterMix.from_text('''\
+        Reagent               Stock        Volume  Master Mix
+        ================  =========   ===========  ==========
+        water                         to 10.00 μL         yes
+        T4 ligase buffer        10x       1.00 μL         yes
+        T4 PNK              10 U/μL       0.25 μL         yes
+        T4 DNA ligase      400 U/μL       0.25 μL         yes
+        DpnI                20 U/μL       0.25 μL         yes
+        PCR product        50 ng/μL       1.50 μL
+        ''')
+
+        kld.num_reactions = self.num_reactions
+        kld.extra_percent = 15
+
+        return kld
+
+    def get_protocol(self):
+        protocol = stepwise.Protocol()
+        protocol += f"""\
+Run {plural(self.num_reactions):# ligation reaction/s}:
+
+{self.reaction}
 
 - Incubate at room temperature for 1h.
 """
+        return protocol
 
-print(protocol)
+if __name__ == '__main__':
+    kld = Kld.from_docopt(__doc__)
+    print(kld.protocol)
