4b7977fe6445c0613de5ed8cffac3f85efbbdefb
Kale Kundert <kale@thekunderts.net>
2021-04-26 16:45:56 -0400

    feat(aliquot): implement freezerbox maker plugin

--- stepwise_mol_bio/aliquot.py
+++ stepwise_mol_bio/aliquot.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import stepwise, appcli, autoprop
+from stepwise import Quantity
 from stepwise_mol_bio import Main
+from freezerbox import MakerArgsConfig, iter_combo_makers, group_by_identity
+from appcli import DocoptConfig, Key
 
 @autoprop
 class Aliquot(Main):
@@ -14,31 +17,71 @@ Usage:
 Arguments:
     <volume>
         The volume of each individual aliquot.  No unit is implied, so you 
-        should specify one.
+        must specify one.
 
     <conc>
         The concentration of the aliquots, if this is not made clear in 
-        previous steps.  No unit is implied, so you should specify one.
+        previous steps.  No unit is implied, so you must specify one.
 """
     __config__ = [
-            appcli.DocoptConfig(),
+            DocoptConfig(),
+            MakerArgsConfig(),
     ]
-    volume = appcli.param('<volume>')
-    conc = appcli.param('<conc>', default=None, ignore=None)
+    volume = appcli.param(
+            Key(DocoptConfig, '<volume>'),
+            Key(MakerArgsConfig, 'volume'),
+    )
+    conc = appcli.param(
+            Key(DocoptConfig, '<conc>'),
+            Key(MakerArgsConfig, 'conc'),
+            default=None,
+            ignore=None,
+    )
 
-    def __init__(self, volume, conc=None):
+    def __bareinit__(self):
+        self.products = []
+        self.show_product_names = False
+
+    def __init__(self, volume, conc=None, products=None):
         self.volume = volume
         self.conc = conc
+        self.products = products or []
+
+    @classmethod
+    def make(cls, db, products):
+        makers_it = iter_combo_makers(
+                cls.from_params,
+                map(cls.from_product, products),
+                group_by={
+                    'volume': group_by_identity,
+                    'conc': group_by_identity,
+                },
+                merge_by={
+                },
+        )
+        makers = list(makers_it)
+        show_product_names = (len(makers) != 1)
+
+        for maker in makers:
+            maker.show_product_names = show_product_names
+            yield maker
 
     def get_protocol(self):
-        p = stepwise.Protocol()
+        Q = Quantity.from_string
 
         if self.conc:
-            p += f"Make {self.volume}, {self.conc} aliquots."
+            aliquot_info = f'{Q(self.volume)}, {Q(self.conc)}'
         else:
-            p += f"Make {self.volume} aliquots."
+            aliquot_info = f'{Q(self.volume)}'
 
-        return p
+        if self.products and self.show_product_names:
+            product_names = f" of: {', '.join(self.products)}"
+        else:
+            product_names = "."
+
+        return stepwise.Protocol(
+                steps=[f"Make {aliquot_info} aliquots{product_names}"],
+        )
 
 if __name__ == '__main__':
     Aliquot.main()
---
+++ tests/test_aliquot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import parametrize_from_file
+from stepwise_mol_bio import ivt
+from schema_helpers import *
+
+@parametrize_from_file(schema=app_expected_protocol)
+def test_protocol(app, expected):
+    actual = app.protocol.format_text()
+    print(actual)
+    for x in expected:
+        assert x in actual
+
+
