f643e66b16e5f0c126c8a7ebf202887aeac39f6c
Kale Kundert <kale@thekunderts.net>
2021-09-16 13:32:03 -0400

    docs(transilluminator): describe config options

--- stepwise_mol_bio/gels/transilluminator.py
+++ stepwise_mol_bio/gels/transilluminator.py
@@ -24,13 +24,32 @@ Arguments:
 
 Options:
     -w --wavelength <nm>
-        The specific wavelength of light to use, in nm.
+        The primary wavelength of light produced by the transilluminator, in 
+        nm.
+
+Configuration:
+    Default values for this protocol can be specified in any of the following 
+    stepwise configuration files:
+
+        ${hanging_indent(app.config_paths, 8)}
+
+    molbio.transilluminator.presets:
+        Named groups of default parameters.  See below for the various settings 
+        that can be specified in each preset.
+
+    molbio.transilluminator.presets.<name>.colloquial_name:
+        How to refer to the transilluminator in the protocol, e.g. "UV" or 
+        "blue-light".
+
+    molbio.transilluminator.presets.<name>.default_wavelength_nm:
+        The default value for the `--wavelength` option.
 """
     __config__ = [
             DocoptConfig,
             PresetConfig,
             StepwiseConfig.setup('molbio.transilluminator'),
     ]
+    config_paths = appcli.config_attr()
     preset_briefs = appcli.config_attr()
 
     presets = appcli.param(
