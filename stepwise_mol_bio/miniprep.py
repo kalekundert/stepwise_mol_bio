c1ba32df9541b5f32b4e39bc4e7b48649861c4c6
Kale Kundert <kale@thekunderts.net>
2022-04-21 21:28:43 -0400

    feat(miniprep): infer yield from plasmid origin

--- stepwise_mol_bio/miniprep.py
+++ stepwise_mol_bio/miniprep.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import autoprop
-from stepwise import Protocol, Quantity
+import appcli
+
+from stepwise import Protocol, Quantity, StepwiseConfig
 from stepwise_mol_bio import Cleanup
-from appcli import DocoptConfig
+from freezerbox import ProductConfig
+from appcli import Key, DocoptConfig
 
 @autoprop
 class Miniprep(Cleanup):
@@ -24,7 +27,34 @@ Database:
 """
     __config__ = [
             DocoptConfig,
+            ProductConfig,
+            StepwiseConfig.setup('molbio.miniprep'),
     ]
+
+    product_ori = appcli.param(
+            Key(ProductConfig, 'origin'),
+    )
+    expected_yield_ng_uL = appcli.param(
+            Key(StepwiseConfig, 'yield_ng_uL'),
+    )
+
+    @classmethod
+    def _combo_maker_factory(cls, db):
+        # `ProductConfig` is automatically loaded for the solo makers, but not 
+        # for the combo makers.  I feel like the `ProductConfig` approach just 
+        # isn't the right way to do this, though.  I think the approach I 
+        # normally take is to:
+        #
+        # - Make a nested class representing the plasmid
+        # - Use `ReagentConfig` within that class to get the info I need.
+        # - If product conc is not ambiguous (i.e. if there is only one 
+        #   plasmid, or all the plasmids have the same ORI), return it.
+        #
+        # Loading the `ProductConfig` here works around the problem in the 
+        # short term, though.
+        app = super()._combo_maker_factory(db)
+        app.load(ProductConfig)
+        return app
 
     def get_protocol(self):
         p = Protocol()
@@ -32,7 +62,13 @@ Database:
         return p
 
     def get_product_conc(self):
-        return Quantity(200, 'ng/µL')
+        yield_ng_uL = self.expected_yield_ng_uL
+
+        if isinstance(yield_ng_uL, dict):
+            key = getattr(self, 'product_ori', 'default')
+            yield_ng_uL = yield_ng_uL.get(key, 'default')
+
+        return Quantity(yield_ng_uL, 'ng/µL')
 
 if __name__ == '__main__':
     Miniprep.main()
